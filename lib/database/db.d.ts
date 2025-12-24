03ee46a739becf46ad25cca13b32f81b0e098787
Rémi Bouteiller <bouteiller.remi@gmail.com>
2025-12-24 16:08:30 +0100

    Follow better auth start documentation

--- lib/database/db.d.ts
+++ lib/database/db.d.ts
@@ -7,9 +7,10 @@ import type { ColumnType } from "kysely";
 
 export type DeviceDisplayMode = "mixup" | "playlist" | "screen";
 
-export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
-	? ColumnType<S, I | undefined, U>
-	: ColumnType<T, T | undefined, T>;
+export type Generated<T> =
+	T extends ColumnType<infer S, infer I, infer U>
+		? ColumnType<S, I | undefined, U>
+		: ColumnType<T, T | undefined, T>;
 
 export type Int8 = ColumnType<
 	string,
