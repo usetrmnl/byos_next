3760f6a7564a9dd1c0f8a25ae7d67dcfb67ac164
scruffy359 <56617491+scruffy359@users.noreply.github.com>
2026-06-22 19:43:20 -0400

    Add unit testing via Jest for helpers module.

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "jest";
+
+const config: Config = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+	testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+	moduleNameMapper: {
+		"^@/(.*)$": "<rootDir>/$1",
+	},
+	transform: {
+		"^.+\\.tsx?$": [
+			"ts-jest",
+			{
+				tsconfig: {
+					rootDir: ".",
+					moduleResolution: "node16",
+					module: "node16",
+				},
+			},
+		],
+	},
+};
+
+export default config;
